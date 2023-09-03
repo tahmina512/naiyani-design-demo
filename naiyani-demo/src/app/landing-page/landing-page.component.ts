a60358ccaac36043cb9bc4a97b2c6f9e0d4febb4
tish001 <tahminatish001@gmail.com>
2023-09-03 16:19:11 +0600

    animation add

--- naiyani-demo/src/app/landing-page/landing-page.component.ts
+++ naiyani-demo/src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('in', style({ opacity: 1 })),
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms ease-in-out'), // Adjust the duration here (500ms)
+      ]),
+      transition(':leave', [
+        animate('500ms ease-in-out', style({ opacity: 0 })), // Adjust the duration here (500ms)
+      ]),
+    ]),
+  ],
 })
 export class LandingPageComponent {
   logoUrl: string = 'assets/images/naiyani-logo.png';
+  logoState = 'in'; // Initial state
   constructor() {}
   ngOnInit(): void {}
 }
